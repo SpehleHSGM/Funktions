face09ca94535e71ab54eab4213e77459c65f8fd
SpehleHSGM <73343218+SpehleHSGM@users.noreply.github.com>
2021-07-12 20:07:06 +0200

    Update dtRead_format.R

--- dtRead_format.R
+++ dtRead_format.R
@@ -1,4 +1,16 @@
+# read
 dt <- fread("https://raw.githubusercontent.com/SpehleHSGM/Datasets/main/datatable.csv",sep = ";",dec = ",",check.names=T)
 
+# format
 dt[,Variante:=as.factor(Variante)][,Wdh:=as.factor(Wdh)][,CO2_Stufe:=as.factor(CO2_Stufe)][,N_Form:=as.factor(N_Form)]
 dt[,Knollenvolumen:=as.numeric(Knollenvolumen)][,Frischmasse.Knolle:=as.numeric(Frischmasse.Knolle)]
+
+# define outlier function: ggplot+geom_boxplot method
+outlier <- function(dp){
+  qt <- quantile(dp,probs=c(0.25,0.75),na.rm = TRUE)
+  iqr <- IQR(dp,na.rm = TRUE)
+  out <- ifelse(dp>(qt[1]-1.5*iqr) & dp<(qt[2]+1.5*iqr),FALSE,TRUE)
+  return(out)
+}
+
+dt[,Flag_out:=outlier(Frischmasse.Knolle), by = Variante]
