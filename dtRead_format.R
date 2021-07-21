885c961da4dbb2d18d0f200d08977c24975e5590
SpehleHSGM <73343218+SpehleHSGM@users.noreply.github.com>
2021-07-21 22:51:01 +0200

    Update dtRead_format.R

--- dtRead_format.R
+++ dtRead_format.R
@@ -30,7 +30,7 @@ dt[,Flag_outlier.mad:=outlier.mad(eval(traitName)),by=interaction(CO2_Stufe,N_Fo
 dt[,Flag_outlier.bonf:=outlierTest(lm(eval(traitName)~CO2_Stufe*N_Form),cutoff=Inf, n.max=Inf,order=FALSE)$bonf.p < 0.05]
 
 # data.table aggregate of trait means by Wdh*CO2_Stufe*N_Form
-dt_means <- dt[ Flag_outlier.iqr==FALSE,.(Knollenvolumen=mean(Knollenvolumen),
+dt_means <- dt[ ,.(Knollenvolumen=mean(Knollenvolumen),
                   Frischmasse.Knolle=mean(Frischmasse.Knolle),
                   count=.N,
                   mainPlots=factor(Wdh:CO2_Stufe)),
