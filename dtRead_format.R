e57eec9d32d14d71d90c0d7d73137b8e3bcf3239
SpehleHSGM <73343218+SpehleHSGM@users.noreply.github.com>
2021-07-21 16:58:06 +0200

    Update dtRead_format.R

--- dtRead_format.R
+++ dtRead_format.R
@@ -9,7 +9,7 @@ dt[,Knollenvolumen:=as.numeric(Knollenvolumen)][,Frischmasse.Knolle:=as.numeric(
 outlier.iqr <- function(dp){
   qt <- quantile(dp,probs=c(0.25,0.75),na.rm = TRUE)
   iqr <- IQR(dp,na.rm = TRUE)
-  out <- ifelse(dp>(qt[1]-3*iqr) & dp<(qt[2]+3*iqr),FALSE,TRUE)
+  out <- ifelse(dp>(qt[1]-1.5*iqr) & dp<(qt[2]+1.5*iqr),FALSE,TRUE)
   return(out)
 }
 # define outlier function: median absolute deviation (MAD)
@@ -28,6 +28,7 @@ dt[,Flag_outlier.mad:=outlier.mad(eval(traitName)),by=interaction(CO2_Stufe,N_Fo
 
 # add column Flag_outlier.bonf (mean shift outlier)
 dt[,Flag_outlier.bonf:=outlierTest(lm(eval(traitName)~CO2_Stufe*N_Form),cutoff=Inf, n.max=Inf,order=FALSE)$bonf.p < 0.05]
+
 # data.table aggregate of trait means by Wdh*CO2_Stufe*N_Form
 dt_means <- dt[ Flag_outlier.iqr==FALSE,.(Knollenvolumen=mean(Knollenvolumen),
                   Frischmasse.Knolle=mean(Frischmasse.Knolle),
