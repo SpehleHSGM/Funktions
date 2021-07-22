5a12f192d67fd6bcd977a7c239cbf94624dba3f1
SpehleHSGM <73343218+SpehleHSGM@users.noreply.github.com>
2021-07-22 22:25:58 +0200

    Update dtRead_format.R

--- dtRead_format.R
+++ dtRead_format.R
@@ -2,7 +2,7 @@
 dt <- fread("https://raw.githubusercontent.com/SpehleHSGM/Datasets/main/datatable.csv",sep = ";",dec = ",",check.names=T)
 
 # format
-dt[,Variante:=as.factor(Variante)][,Wdh:=as.factor(Wdh)][,CO2_Stufe:=as.factor(CO2_Stufe)][,N_Form:=as.factor(N_Form)]
+dt[,Wdh:=as.factor(Wdh)][,CO2_Stufe:=as.factor(CO2_Stufe)][,N_Form:=as.factor(N_Form)]
 dt[,Knollenvolumen:=as.numeric(Knollenvolumen)][,Frischmasse.Knolle:=as.numeric(Frischmasse.Knolle)]
 
 # define outlier function: Whisker by John W. Tukey 
